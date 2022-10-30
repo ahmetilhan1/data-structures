480bba5760083960f1631d91590269a3c87b8150
Fatih-Cyber <71564472+Fatih-Cyber@users.noreply.github.com>
2022-10-30 19:12:22 +0100

    Reverse first k elements in a queue problem added

--- dsa-08-Trees/src/main/java/MyTree.java
+++ dsa-08-Trees/src/main/java/MyTree.java
@@ -12,11 +12,13 @@ public class MyTree {
         TNode current =root;
         while(true){
             if (value<= current.value){
+
                 if (current.leftChild==null){
                     // if left is null insert there!!!
                     current.leftChild=newNode;
                     break;
                 }
+                //if leftChild is not null branch into left subtree!!
                 current=current.leftChild;
             }
             else {
@@ -25,6 +27,7 @@ public class MyTree {
                     current.rightChild=newNode;
                     break;
                 }
+                //if rightChild is not null branch into right subtree!!
                 current=current.rightChild;
             }
         }
